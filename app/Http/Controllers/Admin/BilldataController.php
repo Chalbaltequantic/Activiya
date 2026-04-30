998cf0b7cbb1cf24d8681f719d2375ae556b3012
Chalbaltequantic <121419700+Chalbaltequantic@users.noreply.github.com>
2026-04-30 16:35:55 +0530

    change query condition for freight validate

--- app/Http/Controllers/Admin/BilldataController.php
+++ app/Http/Controllers/Admin/BilldataController.php
@@ -682,7 +682,9 @@ class BilldataController extends Controller
 		$entries = Billdata::whereNotNull('freight_invoice_no')
 				->where('freight_invoice_no', '!=', '')
 				->whereNotNull('freight_invoice_date')
+				->where('freight_invoice_date', '!=', '')
 				->whereNotNull('freight_amount')
+				->whereNotNull('freight_amount', '!=', '')
 				->where('validated_status', '!=', 'submitted')
 				->get();
 					   
