1c1462fcf2ce15a12449cb5491b3c7086501c373
Chalbaltequantic <121419700+Chalbaltequantic@users.noreply.github.com>
2026-04-30 18:16:41 +0530

    update

--- app/Http/Controllers/Admin/BilldataController.php
+++ app/Http/Controllers/Admin/BilldataController.php
@@ -697,10 +697,6 @@ class BilldataController extends Controller
 				->where('freight_invoice_no', '!=', '')
 				->whereNotNull('freight_invoice_date')
 				->where('freight_invoice_date', '!=', '')
-				->where(function ($q) {
-        $q->whereNull('validated_status')
-          ->orWhere('validated_status', '!=', 'submitted');
-    })
 				
 				->get();
 		
