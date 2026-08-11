9469fd693166bad8a7410111f04cc859b4879f9b
Chalbaltequantic <121419700+Chalbaltequantic@users.noreply.github.com>
2026-08-11 20:33:37 +0530

    Update client-quote-b1-r3-revised.blade.php

--- resources/views/admin/spotby/client-quote-b1-r3-revised.blade.php
+++ resources/views/admin/spotby/client-quote-b1-r3-revised.blade.php
@@ -36,19 +36,19 @@
 
     .sticky-col-2 {
       position: sticky;
-      left: 90px; /* Adjust based on col-1 width */
+      left: 80px; /* Adjust based on col-1 width */
       background: #fff;
       z-index: 99;
     }
  .sticky-col-3 {
       position: sticky;
-      left: 200px; /* Adjust based on col-1 width */
+      left: 190px; /* Adjust based on col-1 width */
       background: #fff;
       z-index: 99;
     }
  .sticky-col-4 {
       position: sticky;
-      left: 285px; /* Adjust based on col-1 width */
+      left: 275px; /* Adjust based on col-1 width */
       background: #fff;
       z-index: 99;
     }
