d0af45e6402ddc62989ac476b060fb7ecbb43d75
Chalbaltequantic <121419700+Chalbaltequantic@users.noreply.github.com>
2026-08-11 20:30:44 +0530

    Update spotby-vendor-quote-round2.blade.php

--- resources/views/admin/spotby/spotby-vendor-quote-round2.blade.php
+++ resources/views/admin/spotby/spotby-vendor-quote-round2.blade.php
@@ -42,7 +42,7 @@
     }
  .sticky-col-3 {
       position: sticky;
-      left: 1900px; /* Adjust based on col-1 width */
+      left: 190px; /* Adjust based on col-1 width */
       background: #fff;
       z-index: 99;
     }
