20747eb6399efcfda5fd06f1a86e768a40cbb25f
Chalbaltequantic <121419700+Chalbaltequantic@users.noreply.github.com>
2026-08-13 18:24:08 +0530

    Update spotby-select-vendor.blade.php

--- resources/views/admin/spotby/spotby-select-vendor.blade.php
+++ resources/views/admin/spotby/spotby-select-vendor.blade.php
@@ -47,7 +47,7 @@
     }
  .sticky-col-3 {
       position: sticky;
-      left: 200px; /* Adjust based on col-1 width */
+      left: 190px; /* Adjust based on col-1 width */
       background: #fff;
       z-index: 99;
     }
